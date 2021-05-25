3704fc7d6997ffd96c1c1aa24ba4f28a6be79955
wrkzk <warren.john.kozak@gmail.com>
2021-05-25 11:30:18 -0500

    Add code to automatically calculate a reverse path

--- text_read.py
+++ text_read.py
@@ -6,7 +6,21 @@ def read_file():
         for line in param_file:
             if line[0] == "P":
                 movements = line[2:].strip().split(',')
-                return movements
+                movements_reverse = movements.copy()
+                movements_reverse.reverse()
                 
+                i = 0
+                for x in range(0, len(movements_reverse)):
+                    if movements_reverse[i] == "R":
+                        movements_reverse[i] = "L"
+                    elif movements_reverse[i] == "L":
+                        movements_reverse[i] = "R"
+                    i += 1
+
+                movements.append("TA")
+                movements.extend(movements_reverse)
+                movements.append("TA")
+                return movements
+                                
 if __name__ == "__main__":
     print(read_file())
